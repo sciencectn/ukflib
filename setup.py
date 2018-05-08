7f4f230787c6b3a8c634d42d5a8aae1973e42030
Chris Taylor <1373858+sciencectn@users.noreply.github.com>
2018-05-08 17:12:37 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup
+
+
+setup(
+    name="ukflib",
+    author="Chris Taylor",
+    author_email="sciencectn@gmail.com",
+    description="An Unscented Kalman Filter library that allows for nonadditive process and measurement noise",
+    keywords="ukf unscented kalman filter",
+    version="0.0.1",
+    py_modules="ukflib",
+    install_requires=["numpy>=1.14.0",
+                      "matplotlib>=2.2.0",
+                      "scipy>=1.0.0"],
+    url="https://github.com/sciencectn/ukflib"
+)
+
+
